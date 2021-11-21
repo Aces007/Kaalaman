ed8b3ec786e8a9321fe9eb65e97e59941170c3de
Ace Lawrence <clavanoace@gmail.com>
2021-11-20 21:41:55 -0800

    program 1 editing in progress

--- 1st.py
+++ 1st.py
@@ -1,3 +1,5 @@
+# Are you looking to sort the sequence of your desired numbers?, my program does that, all you have to do is input 4 numbers, and
+# my program will do all the work.  
 def PengeNgIyongNumero():
     NumeroUno = int(input("Enter the first number you wish to sort: "))
     NumeroDos = int(input("Enter the second number you wish to sort: "))
@@ -5,19 +7,21 @@ def PengeNgIyongNumero():
     NumeroKwatro = int(input("Enter the fourth number you wish to sort: "))
     return NumeroUno, NumeroDos, NumeroTres, NumeroKwatro
 
-
+# This will be the function responsible for sorting your numbers from highest to lowest. We will use the return values below for this 
+# function. 
 def SortingTime():
     SinoBaPinakaMataas = max(Una, Pangalawa, Pangatlo, Ikaapat)
 
-
+    # This if/else statement will verify if your second no. is greater than the first. But if "not"
     if Pangalawa > Una:
         PinakamababaSi = Una
         SecondSi = Pangalawa
+    # This else statement will execute the opposite value that did not became True on the if statement.    
     else:
         PinakamababaSi = Pangalawa
         SecondSi = Una
 
-
+    
     if Pangatlo < Pangalawa:
         PangatloSi = SecondSi
         SecondSi = PinakamababaSi
@@ -64,8 +68,9 @@ def SortingTime():
     print (f"So, based on the numbers you entered; from highest to lowest, the arrangement of the list is {PinakamataasSi}, {PangatloSi}, {SecondSi}, {PinakamababaSi}")
 
 
-
+# This right here will return the 4 numbers you entered above, they are essential for the effectiveness of the program.
 Una, Pangalawa, Pangatlo, Ikaapat = PengeNgIyongNumero() 
 
 
+# This variable will return all the operations included in SortingTime(), which means all if/else statements are involved. 
 PakiArrange = SortingTime()
